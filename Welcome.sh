4ba99fca2d472f2c2b6b0c5f5720ddbee0f973f7
Hari <hari@javahome.in>
2017-03-09 16:55:47 +0530

    Git practice

--- Welcome.sh
+++ Welcome.sh
@@ -1,16 +1,12 @@
-# devops-4pm-march
-Welcome to GIT tutorial
-
-Changes added by developer2
-
-Changes done by Hari
-
-Changes done by Developer-2
-
-Fixed defect-1
-
-some change
-
-some change
-
-some change
+1) Crate a new repository in github
+2) Clone the repository into local machine
+3) Create a bracnch with name 'feature-1'
+4) Chekout 'feature-1' and
+4) add new file mydetails.txt
+        -- Enter your name & phone
+5) Stage & commit the changesin 'feature-1'.
+6) Push 'feature-1' to remote
+7) Create a pull request 'master' 'feature-1'
+8) Merge to master & close the pull request
+9) Delete 'feature-1' in remote & local
+10) create a tag with name RELEASE-1.0.0 on 'master'
